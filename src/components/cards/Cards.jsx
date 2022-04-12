4509ea187ae529d1a56b251f18f6dfc5fb4905c2
Dima-Front <dmitry.k@bostil-support.com>
2022-04-12 15:03:36 +0300

    a little refactoring

--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -8,12 +8,16 @@ import it from './cards.module.css'
 function Cards({repos}) {
     const load = useSelector(state => state.cardReducer.isLoading)
 
+    if (load) {
+        return <div style={{alignItems: "center", marginTop: '25px'}}><Loader/></div>
+    }
+
     return (
         <div className={it.items}>
-            {!load ? repos ? repos.map(repo => <CardItem key={repo.id} watchers={repo.watchers}
-                                                         star={repo.stargazers_count} name={repo.name}
-                                                         html_url={repo.html_url}/>) : 'нет данных' :
-                <div style={{alignItems: "center", marginTop: '25px'}}><Loader/></div>}
+            {repos ? repos.map(repo => <CardItem key={repo.id} watchers={repo.watchers}
+                                                 star={repo.stargazers_count} name={repo.name}
+                                                 html_url={repo.html_url}/>) : 'нет данных'
+            }
         </div>
     );
 }
